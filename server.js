e6c5852f79ce3409652ecc41697b1306e37af4af
tizzy273 <tiziano.riccioli@gmail.com>
2024-09-10 20:05:16 +0200

    server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ let express = require('express')
 
 let app = express();
 
-app.use(express.static(__dirname + 'dist/source-code/browser'));
+app.use(express.static(__dirname + '/dist/source-code/browser'));
 
 
 app.get('/*', (req, resp)=>{
-    resp.sendFile(__dirname + 'dist/source-code/browser/index.html')
+    resp.sendFile(__dirname + '/dist/source-code/browser/index.html')
 });
 
 
