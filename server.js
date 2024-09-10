eaf488c0004d4229e18553be82d7af776fbfab59
tizzy273 <tiziano.riccioli@gmail.com>
2024-09-10 19:44:35 +0200

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express')
+
+let app = express();
+
+app.use(express.static(__dirname + 'dist/assignment-fe'));
+
+
+app.get('/*', (req, resp)=>{
+    resp.sendFile(__dirname + 'dist/assignment-fe/index.html')
+});
+
+
+app.listen(process.env.PORT);       
